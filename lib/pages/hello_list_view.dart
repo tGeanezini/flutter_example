c73d7e0d7950523f545488b2382eb433e2edf9f7
Tiago Geanezini <t.geanezini@gmail.com>
2020-08-28 16:51:46 -0300

    Converts list page to StatefulWidget

--- lib/pages/hello_list_view.dart
+++ lib/pages/hello_list_view.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example/model/dog.dart';
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _isGridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              setState(() {
+                _isGridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              setState(() {
+                _isGridView = true;
+              });
+            },
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -21,36 +46,51 @@ class HelloListView extends StatelessWidget {
       Dog('Border Collie', 'assets/images/dog5.png'),
     ];
 
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemCount: dogs.length,
-      itemBuilder: (context, index) {
-        final dog = dogs[index];
-        return Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            _img(dog.photo),
-            Container(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Text(
-                  dog.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
+    if (_isGridView) {
+      return GridView.builder(
+        gridDelegate:
+        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: dogs.length,
+        itemBuilder: (context, index) {
+          return _buildItemView(dogs, index);
+        },
+      );
+    } else {
+      return ListView.builder(
+        itemCount: dogs.length,
+        itemExtent: 300,
+        itemBuilder: (context, index) {
+          return _buildItemView(dogs, index);
+        },
+      );
+    }
+  }
+
+  Stack _buildItemView(List<Dog> dogs, int index) {
+    final dog = dogs[index];
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        _img(dog.photo),
+        Container(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              dog.name,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 
